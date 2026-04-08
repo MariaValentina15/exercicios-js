3590dc813a90ab6cef0aaf6f94c4baf9b929613e
MariaValentina15 <mvs19@aluno.ifnmg.edu.br>
2026-04-08 22:53:41 +0000

    feat: adiciona contar letra

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -41,5 +41,11 @@ return soma;
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contador = 0;
+  for (const caractere of texto) {
+    if (caractere === letra) {
+      contador += 1;
+    }
+  }
+  return contador;
 }
